0a310126730c9433c18d3ad9ccc2d252d1f53466
squarestat <vsds2020@gmail.com>
2026-03-12 15:13:40 -0300

    Create coleta_instagram.R

---
+++ coleta_instagram.R
@@ -0,0 +1,41 @@
+library(httr)
+library(jsonlite)
+
+get_followers <- function(username){
+
+  url <- paste0(
+    "https://i.instagram.com/api/v1/users/web_profile_info/?username=",
+    username
+  )
+
+  res <- httr::GET(
+    url,
+    httr::add_headers(
+      "User-Agent" = "Mozilla/5.0",
+      "x-ig-app-id" = "936619743392459"
+    )
+  )
+
+  data <- jsonlite::fromJSON(httr::content(res, "text", encoding = "UTF-8"))
+
+  data$data$user$edge_followed_by$count
+}
+
+users <- c("fgvibre")
+
+followers <- sapply(users, get_followers)
+
+dados <- data.frame(
+  data = Sys.Date(),
+  username = users,
+  followers = followers
+)
+
+arquivo <- "followers_historico.csv"
+
+if(file.exists(arquivo)){
+  antigo <- read.csv(arquivo)
+  dados <- rbind(antigo, dados)
+}
+
+write.csv(dados, arquivo, row.names = FALSE)
